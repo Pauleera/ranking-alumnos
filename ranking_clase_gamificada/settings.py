dbdda4c0400136dd467f70daadb08f2988a08e50
Pauleera <pfma98@gmail.com>
2025-06-06 14:40:21 -0400

    Fix: Re-add AuthenticationMiddleware for Django Admin

--- ranking_clase_gamificada/settings.py
+++ ranking_clase_gamificada/settings.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
